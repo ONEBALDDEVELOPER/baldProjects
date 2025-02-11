95613de51a596e9611f8a08de24ce449a08e02de
onebalddeveloper <andyiwebeluajarret@gmail.com>
2025-02-11 01:42:41 +0000

    this is a JS tutorial file

---
+++ FreecodeCamp/tutorial.js
@@ -0,0 +1,62 @@
+// let firstName = 'Jarret';
+// console.log(firstName)
+
+
+    // Initializing Data Types
+    // ```````````````````````
+    // -------- Strings -------
+    // let favFruit = 'Orange';
+    // let favSports = "Football";
+    // let favActivity = `Coding`; creates a template literal
+
+    // -------- Numbers(int) --------
+    // let age = 25;
+    // let height = 1.75; // decimal number or float
+    // let accBalance = 750400250109n; //huge numbers
+
+    // ------- Boolean --------
+    // let lovesCoding = true;
+
+    //------- Null ----------- This is used to explicitly clear the value of a variable
+    // let name = null;
+
+    // ------ Symbols -------
+    // let sym = Symbol('id');
+
+    // ----- Objects -------- These are reference type datas
+    // let person = {}
+    // let person = {name: 'John', age: 30, occupation: 'Software Developer'};
+
+
+// let course = {
+//     name: 'JavaScript',
+//     price: 199.99,
+//     isItFree: false
+// };
+// console.log(course.name);
+// console.log(course.price);
+// console.log(course.isItFree);
+// console.log(course); // prints the whole object
+
+// console.log(course['name']); //using bracket notation to print the same thing as the dot notation
+
+// console.log(course['price']);
+
+//--------- Arrays ------------
+// let students = ['John', 'Jane', 'Bob'];
+// let favNum = [1, 2, 3, 4];
+// let mixed = ['apple', 2, true, null, undefined, {petsName: 'Bully'}];
+//console.log(mixed);
+// console.log(mixed[5]);
+
+//------- Function ----------
+// function greet(name) {
+//     console.log(`Hello, ${name}!`);
+// }
+// greet('Chike');
+
+// function multiply(num1, num2){
+//     return num1 * num2;
+// }
+
+// console.log( multiply(2, 3) );
